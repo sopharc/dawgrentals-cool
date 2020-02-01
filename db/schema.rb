9ad709f69e2c9f74b316ff7df023cde938b3461d
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2020-02-01 16:24:54 +0000

    Review model basics added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_01_160829) do
+ActiveRecord::Schema.define(version: 2020_02_01_162301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,15 @@ ActiveRecord::Schema.define(version: 2020_02_01_160829) do
     t.index ["user_id"], name: "index_rentals_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.bigint "rental_id"
+    t.integer "rating"
+    t.string "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["rental_id"], name: "index_reviews_on_rental_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -52,4 +61,5 @@ ActiveRecord::Schema.define(version: 2020_02_01_160829) do
 
   add_foreign_key "rentals", "dogs"
   add_foreign_key "rentals", "users"
+  add_foreign_key "reviews", "rentals"
 end
