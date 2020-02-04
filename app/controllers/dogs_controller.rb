2543310b5320bc67511d7a5d9d2af1c92e68b6b5
Sophie Archer <hello@sophiearcher.co.uk>
2020-02-04 19:57:31 +0000

    dog index added

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,7 +1,15 @@
 class DogsController < ApplicationController
   def index
+    @dogs = Dog.all
   end
 
   def show
+    @dogs = Dog.find(params[:id])
   end
+
+  # private
+
+  # def dog_params
+  #   params.require(:dog).permit(:name, :breed, :bio, :photo)
+  # end
 end
