cb59286c4c6e10b92f6110825a79dc6f58ab19fc
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2020-02-04 21:16:56 +0000

    4 users and 10 dogs seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,76 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# CREATE 5 DOGS
-max = Dog.create(name: "Max", breed: "Husky", bio: "Hi there! I'm the best dog EVER! WOOF!")
-lady = Dog.create(name: "Lady", breed: "Poodle", bio: "Look at me look at me! I'm so prettyyyy!!! BARK BARK")
-piper = Dog.create(name: "Piper", breed: "Great dane", bio: "What are you looking at!? I'm not going to eat you! DJEEZ")
-bandit = Dog.create(name: "Bandit", breed: "Bulldog", bio: "I'm so so so sweet but I'll drool ALL over you! Sorry not sorry!")
-jester = Dog.create(name: "Jester", breed: "Cocker spaniel", bio: "Whatever man - I'm the best!")
+require 'faker'
 
-# CREATE 5 USERS MANUALLY
+def getName
+  name = Faker::Creature::Dog.name.split(" ")[0]
+  name.length >= 4 ? name : getName
+end
+
+def getBreed
+  breeds = [
+    "Husky",
+    "Jack Russel",
+    "Doberman Pincher",
+    "Dachshund (sausage dog)",
+    "Weimaraner",
+    "Welsh Terrier",
+    "Cavachon",
+    "Dandie Dinmont Terrier",
+    "Labradoodle",
+    "Miniature Schnauzer",
+    "Flat-coated Retriever",
+    "Golden Retriever",
+    "German Shepherd",
+    "Border Collie",
+    "Boxer",
+    "Cocker Spaniel",
+    "Springer Spaniel",
+    "Labrador Retriever",
+    "Cockapoo",
+    "Staffordshire Bull Terrier (staffie)"
+  ].sample
+end
+
+def getBio
+  bio = [
+    "Hi there! I'm the best dog EVER! WOOF!",
+    "Look at me look at me! I'm so prettyyyy!!! BARK BARK",
+    "What are you looking at!? I'm not going to eat you! DJEEZ",
+    "I'm so so so sweet but I'll drool ALL over you! Sorry not sorry!",
+    "Whatever man - I'm the best!"
+  ].sample
+end
+
+def getPostcode
+  postcode = [
+    "E14 9GZ",
+    "E17 5HL",
+    "E17 5JF",
+    "E5 9AB",
+    "E17 0ZZ",
+    "E7 8EW",
+    "EC4Y 0DY",
+    "EC1P 1EE",
+    "EC2Y 9AA",
+    "EC1A 1UD",
+    "N9 0BG",
+    "N8 9NT",
+    "N6 6EH",
+    "N2 0FA",
+    "N19 3YQ"
+  ].sample
+end
 
 
-# CREATE RENTALS
+User.create(name: "Jonas", email: "jonas@gmail.com")
+User.create(name: "Sophie", email: "sophie@gmail.com")
+User.create(name: "Jamie", email: "jamie@gmail.com")
+User.create(name: "Tom", email: "tom@gmail.com")
 
-
-# CREATE REVIEWS
+10.times do
+  Dog.create(name: getName, breed: getBreed, bio: getBio, location: getPostcode, user: User.find(rand(1..4)))
+end
 
 
